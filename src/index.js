088243c6ba93725273c5151615d1e44bf7106e8b
David Konsumer <konsumer@jetboystudio.com>
2017-07-13 09:58:57 -0700

    buggy service worker

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux'
 
 import './index.css'
 import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
 import store from './store'
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
-registerServiceWorker()
