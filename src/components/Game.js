40f119c1d1a6fc7a7363f32e28124062df8b57f7
David Konsumer <konsumer@jetboystudio.com>
2017-07-13 11:39:01 -0700

    better handling of winner/losers

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,11 +5,17 @@ import './Game.css'
 
 import Card from './Card'
 
-export const Game = ({ showing, hands }) => (<section className='Game'>
-  {showing.map((c, i) => <div key={i} className='player'>
-    {hands[i].length ? <Card card={c} /> : <div className='Card red out'>✘</div>}
-    player {i + 1} ({hands[i].length ? hands[i].length + 1 : 0})
-  </div>)}
+export const Game = ({ showing, hands, numberOfPlayers }) => (<section className='Game'>
+  {showing.map((c, i) => (hands[i].length
+    ? (<div key={i} className='player'>
+      <Card card={c} />
+      player {i + 1} ({hands[i].length >= (52 - numberOfPlayers) ? 'winner' : hands[i].length + 1})
+    </div>)
+    : (<div key={i} className='player'>
+      <div className='Card red out'>✘</div>
+      player {i + 1} (loser)
+    </div>)
+  ))}
 </section>)
 
 export default connect(state => state, dispatch => ({}))(Game)
